ee57ce8946a1372bc854fd1e32834a1749039130
Kvarutar <PandaPimp@yandex.ru>
2020-01-24 16:24:15 +0300

    функции и циклы

--- script1.js
+++ script1.js
@@ -13,12 +13,56 @@ let appDate =
     savings: false
 };
 
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце"),
-    answer2 = prompt("Во сколько обойдется?");
+/*for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && a != '' && b != '' ){
+        console.log(typeof(a));
+        appDate.expenses[a] = b;
+    }else {
+        alert("Введено некорректное значение");
+        i--;
+    }
+}*/
 
-appDate.expenses.answer1 = answer2;
+let j = 0;
 
-alert(appDate.budget/30);
+/*while (j < 2){
+    j++;
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && a != '' && b != '' ){
+        console.log(typeof(a));
+        appDate.expenses[a] = b;
+    }else {
+        alert("Введено некорректное значение");
+        i--;
+    }
+}*/
 
-console.log(appDate.timeDate);
-console.log(appDate.expenses.answer1);
+j=0;
+do {
+    j++;
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ( (typeof(a)) === 'string' && (typeof(b)) === 'string' && a != '' && b != '' ){
+        console.log(typeof(a));
+        appDate.expenses[a] = b;
+    }else {
+        alert("Введено некорректное значение");
+        i--;
+    } 
+} while (j < 2);
+
+appDate.moneyPerDay = appDate.budget/30;
+alert(appDate.moneyPerDay);
+
+if (appDate.moneyPerDay < 100){
+    console.log("Низий достаток");
+}else if(appDate.moneyPerDay > 100 && appDate.moneyPerDay < 2000){
+    console.log("Средний достаток");
+}else if (appDate.moneyPerDay > 2000){
+    console.log("Высокий достаток");
+}else {
+    console.log("Ошибка");
+}
