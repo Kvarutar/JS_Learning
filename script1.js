a0f264f3cac5e9b6e9f3be92677ed2ad36f9e37d
Kvarutar <PandaPimp@yandex.ru>
2020-01-24 15:27:11 +0300

    first zadanie

--- script1.js
+++ script1.js
@@ -1,4 +1,24 @@
-//console.log("arr" + "- object");
 
-let answer = +prompt("your age");
-console.log(typeof(answer));
+
+let money = +prompt("Ваш бюджет на месяц?");
+let date = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appDate = 
+{
+    budget: money,
+    timeDate: date,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    answer2 = prompt("Во сколько обойдется?");
+
+appDate.expenses.answer1 = answer2;
+
+alert(appDate.budget/30);
+
+console.log(appDate.timeDate);
+console.log(appDate.expenses.answer1);
