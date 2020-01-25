60a50b699d9f120ba0ef21669b32c0909bcc30b3
Kvarutar <PandaPimp@yandex.ru>
2020-01-25 19:05:01 +0300

    Доработка приложения

---
+++ Function.js
@@ -0,0 +1,10 @@
+//function calc(a, b) {
+//    return(a+b);
+//}
+
+let calc = (a,b) => a+b;//!!!Новый стандарт
+//alert(calc(10, 5));
+
+let str = "test";
+console.log(str.length)//.lenght - свойство!
+console.log(str.toUpperCase())//- функция!
